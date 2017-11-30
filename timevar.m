4bf98fc5698200c12ceaf00d42b28a1aa54a80f1
zac <zac.kemp@outlook.com>
2017-11-30 13:03:43 +1100

    fixed typo

--- timevar.m
+++ timevar.m
@@ -15,7 +15,8 @@ counts = linspace(0.1e2, 1e2, n_counts);
 for lambda_index = 1:n_lambdas
     for count_index = 1:n_counts
         std_time(lambda_index, count_index) = ...
-            calculate_standard_deviation(N0, lambdas(lambda_index), counts(count_index))
+            calculate_standard_deviation(N0, lambdas(lambda_index),... 
+                                         counts(count_index))
         std_time_theor(lambda_index, count_index) = ...
             sqrt(counts(count_index)) / (lambdas(lambda_index) * N0)
     end
@@ -31,6 +32,7 @@ hold on
 mesh(X, Y, std_time_theor, 'EdgeColor', 'b')
 xlabel('lambda')
 ylabel('total counts')
+zlabel('\sigma_{time}')
 
 function [stdev] = calculate_standard_deviation(N0, lambda, counts_limit)
     % returns the standard deviation in times measured for counts_limit
